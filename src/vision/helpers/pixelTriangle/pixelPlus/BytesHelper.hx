75a90627c14bc589bf744fa920728e4b20cdc308
nanjizal <nanjizal@users.noreply.github.com>
2022-10-26 10:56:30 +0100

    create BytesHelper

---
+++ src/vision/helpers/pixelTriangle/pixelPlus/BytesHelper.hx
@@ -0,0 +1,19 @@
+package vision.helpers.pixelTriangle.pixelPlus;
+
+/**
+ Allows injection of Bytes data directly into the Image and direct extraction
+ **/
+@:access( vision.ds.Image.OFFSET )
+final imageOff = vision.ds.Image.OFFSET;
+inline 
+function imageLen( image: vision.ds.Image ): Int
+    return image.width * image.height * 4;
+inline
+function injectBytesInImage( bytes: haxe.io.Bytes, image: vision.ds.Image )
+    image.underlying.blit( imageOff, bytes, 0, imageLen( image ) );
+inline
+function extractBytesFromImage( image: vision.ds.Image ): haxe.io.Bytes {
+    var out = haxe.io.Bytes.alloc( imageLen( image ) );
+    out.blit( 0, image.underlying, imageOff, imageLen( image ) );
+    return out;
+}
