e38c0dbf643d3ad607ae53dba411b3f6facd3233
nanjizal <nanjizal@users.noreply.github.com>
2022-10-26 11:09:44 +0100

    Update BoundIterator.hx

--- src/vision/helpers/pixelTriangle/iter/BoundIterator.hx
+++ src/vision/helpers/pixelTriangle/iter/BoundIterator.hx
@@ -21,7 +21,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
-package nanjizal.visionTriangle.iter;
+package vision.helpers.pixelTriangle.iter;
 
 /**
  Used for bounding box iteration, calculates lo...hi iterator from 3 values. 
