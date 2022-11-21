9840a871622b2932cc3419ecb296dfa6ccba922e
ShaharMS <shaharmsecond@gmail.com>
2022-11-21 10:24:21 +0200

    another attempt at fixing OutsideBounds error

--- src/vision/algorithms/SimpleLineDetector.hx
+++ src/vision/algorithms/SimpleLineDetector.hx
@@ -33,7 +33,8 @@ class SimpleLineDetector {
 
 
 	public static function findLineFromPoint(point:Int16Point2D, minLineLength:Float, maxGap:Int = 1):Line2D {
-		if (!image.hasPixel(point.x, point.y) || image.getUnsafePixel(point.x, point.y) != 0xFFFFFFFF || cachedPoints.contains(point)) return null;
+		if (!image.hasPixel(point.x, point.y) ) return null;
+		if (image.getUnsafePixel(point.x, point.y) != 0xFFFFFFFF || cachedPoints.contains(point)) return null;
 		final start = p(point.x, point.y);
 		var pointCheckOrder:Array<Int16Point2D> = [p(1, 0), p(1, 1), p(1, -1), p(-1, 0), p(-1, 1), p(-1, -1), p(0, 1), p(0, -1)];
 		var cwp:Null<Int16Point2D> = p(point.x, point.y);
