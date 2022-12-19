561395bc210b644e4ee8d850750c5bd0c9a5344e
Wither362 <93864752+Wither362@users.noreply.github.com>
2022-12-19 13:20:24 +0100

    Update RoundedRectangle.hx

--- src/vision/ds/figures/RoundedRectangle.hx
+++ src/vision/ds/figures/RoundedRectangle.hx
@@ -23,5 +23,23 @@ class RoundedRectangle /*extends Rectangle*/ {
 	/**
 		How rounded are the corners of this `RoundedRectangle`
 	**/
-	public var round:Float;
+	public var rounded:Float;
+
+	public var accumulatedPoints:Array<{x:Int, y:Int}> = [];
+
+	public function new(x:Int = 0, y:Int = 0, width:Int, height:Int, rounded:Float, accumulatePoints:Bool = true) {
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+
+		if(accumulatePoints) {
+			for (x in 0...this.width) {
+				for (y in 0...this.height) {
+					accumulatedPoints.push({x: x, y: y});
+				}
+			}
+			for (pointt in accumulatedPoints)
+				trace('RoundedRectangle: Pixel added at (x: ' + pointt.x + ', y: ' + pointt.y + ')');
+		}
 }
