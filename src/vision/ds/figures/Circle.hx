292c7165dd2e39f24ae8b1f10a17b33a9504c3ab
Wither362 <93864752+Wither362@users.noreply.github.com>
2022-12-19 16:02:26 +0100

    Create Circle.hx

---
+++ src/vision/ds/figures/Circle.hx
@@ -0,0 +1,23 @@
+package vision.ds.figures;
+
+@:structInit
+class Circle {
+	/**
+		The `x` position of this `Rectangle`
+	**/
+	public var x:Int;
+	/**
+		The `y` position of this `Rectangle`
+	**/
+	public var y:Int;
+	/**
+		The `width` of this `Rectangle`
+	**/
+	public var width:Int;
+	/**
+		The `height` of this `Rectangle`
+	**/
+	public var height:Int;
+
+	public var radius:Int;
+}
