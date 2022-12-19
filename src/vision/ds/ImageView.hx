80a0d606c35870b4b0f4d0a879fc20977d40099b
Wither362 <93864752+Wither362@users.noreply.github.com>
2022-12-19 13:05:48 +0100

    Update ImageView.hx

--- src/vision/ds/ImageView.hx
+++ src/vision/ds/ImageView.hx
@@ -5,10 +5,11 @@ class ImageView {
     @:optional public var x:Int = 0;
     @:optional public var y:Int = 0;
     @:optional public var width:Int = 0;
-    @:optional public var height:Int = 0; 
+    @:optional public var height:Int = 0;
+    @:optional public var rounded:Float = 0;
     @:optional public var shape:ImageViewShape = RECTANGLE;
 
     public function toString() {
-        return '{shape: $shape, x: $x, y: $y, width: $width, height: $height}';
+        return '{shape: $shape, x: $x, y: $y, width: $width, height: $height, rounded: $rounded}';
     }
 }
