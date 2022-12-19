f11b0507ec2b392faac3abee7de244f8a313479e
Wither362 <93864752+Wither362@users.noreply.github.com>
2022-12-19 16:08:59 +0100

    Update Circle.hx

--- src/vision/ds/figures/Circle.hx
+++ src/vision/ds/figures/Circle.hx
@@ -20,4 +20,19 @@ class Circle {
 	public var height:Int;
 
 	public var radius:Int;
+
+	public function coordinateExists(X:Int, Y:Int):Bool {
+		var locatedCoordinates:Array<{x:Int, y:Int}> = [];
+		var rectangleCoordinates:Array<{x:Int, y:Int}> = [];
+
+		for (x in 0...width) {
+			for (y in 0...height) {
+				rectangleCoordinates.push({x: x, y: y});
+			}
+		}
+
+		
+
+		return false;
+	}
 }
