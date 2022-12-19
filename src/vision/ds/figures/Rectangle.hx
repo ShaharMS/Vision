ab5c23ea9a4415a81e53dd1ecfcf6e142f6d9f60
Wither362 <93864752+Wither362@users.noreply.github.com>
2022-12-19 12:31:15 +0100

    Create Rectangle.hx

---
+++ src/vision/ds/figures/Rectangle.hx
@@ -0,0 +1,21 @@
+package vision.ds.figures;
+
+@:structInit
+class Rectangle {
+	/**
+		The `x` position of this `Rectangle`
+	**/
+	public var x:Int;
+	/**
+		The `y` position of this `Rectangle`
+	**/
+	public var y:Int;
+	/**
+		The `width` of this `Rectangle`
+	**/
+	public var width:Int;
+	/**
+		The `height` of this `Rectangle`
+	**/
+	public var height:Int;
+}
