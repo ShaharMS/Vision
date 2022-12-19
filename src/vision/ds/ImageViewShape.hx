81f0e728cd769d6c70fb5ecb907744dc9dc19b4e
Wither362 <93864752+Wither362@users.noreply.github.com>
2022-12-19 12:18:31 +0100

    Rounded shapes

--- src/vision/ds/ImageViewShape.hx
+++ src/vision/ds/ImageViewShape.hx
@@ -1,8 +1,12 @@
 package vision.ds;
 
 enum abstract ImageViewShape(Int) from Int to Int {
+    var SQUARE;
+    var ROUNDED_SQUARE;
     var RECTANGLE;
+    var ROUNDED_RECTANGLE;
     var RECTANGLE_INVERTED;
+    var ROUNDED_RECTANGLE_INVERTED;
     var ELLIPSE;
     var ELLIPSE_INVERTED;
     var RHOMBUS;
