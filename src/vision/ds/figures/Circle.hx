1689fab3c8d20ef6bb4037c05e7bff8a646180e5
Wither362 <93864752+Wither362@users.noreply.github.com>
2022-12-19 16:15:38 +0100

    Update Circle.hx

--- src/vision/ds/figures/Circle.hx
+++ src/vision/ds/figures/Circle.hx
@@ -3,36 +3,38 @@ package vision.ds.figures;
 @:structInit
 class Circle {
 	/**
-		The `x` position of this `Rectangle`
+		The `x` position of this `Circle`
 	**/
 	public var x:Int;
 	/**
-		The `y` position of this `Rectangle`
+		The `y` position of this `Circle`
 	**/
 	public var y:Int;
 	/**
-		The `width` of this `Rectangle`
+		The `width` of this `Circle`
 	**/
 	public var width:Int;
 	/**
-		The `height` of this `Rectangle`
+		The `height` of this `Circle`
 	**/
 	public var height:Int;
 
 	public var radius:Int;
 
 	public function coordinateExists(X:Int, Y:Int):Bool {
-		var locatedCoordinates:Array<{x:Int, y:Int}> = [];
-		var rectangleCoordinates:Array<{x:Int, y:Int}> = [];
+		var locatedCoords:Array<{x:Int, y:Int}> = [];
+		var rectangleCoords:Array<{x:Int, y:Int}> = [];
 
 		for (x in 0...width) {
 			for (y in 0...height) {
-				rectangleCoordinates.push({x: x, y: y});
+				rectangleCoords.push({x: x, y: y});
 			}
 		}
+		for(haha in rectangleCoords) {
+			if((haha.x % radius) == 0 && (haha.y % radius) == 0)
+				locatedCoords.push(haha);
+		}
 
-		
-
-		return false;
+		return locatedCoords.contains({x: X, y: Y});
 	}
 }
