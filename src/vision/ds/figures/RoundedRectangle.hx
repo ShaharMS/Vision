d7440b92e3d6f7eb51ccce9d4451f5953e940cac
Wither362 <93864752+Wither362@users.noreply.github.com>
2022-12-19 12:36:34 +0100

    Hm….

---
+++ src/vision/ds/figures/RoundedRectangle.hx
@@ -0,0 +1,27 @@
+package vision.ds.figures;
+
+//import vision.ds.figures.Rectangle;
+
+@:structInit
+class RoundedRectangle /*extends Rectangle*/ {
+	/**
+		The `x` position of this `RoundedRectangle`
+	**/
+	public var x:Int;
+	/**
+		The `y` position of this `RoundedRectangle`
+	**/
+	public var y:Int;
+	/**
+		The `width` of this `RoundedRectangle`
+	**/
+	public var width:Int;
+	/**
+		The `height` of this `RoundedRectangle`
+	**/
+	public var height:Int;
+	/**
+		How rounded are the corners of this `RoundedRectangle`
+	**/
+	public var round:Float;
+}
