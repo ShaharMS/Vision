d2b6fd7e128a16928df4a1d965ec676d74c3cd8b
Wither362 <93864752+Wither362@users.noreply.github.com>
2023-01-10 14:06:02 +0100

    Update Test_vision_Vision_skew.hx

--- unit_tests/skewImage/src/Test_vision_Vision_skew.hx
+++ unit_tests/skewImage/src/Test_vision_Vision_skew.hx
@@ -1,8 +1,31 @@
 package;
 
+import vision.Image;
+import vision.ds.ImageTools;
+
+using vision.Vision;
 
 class Test_vision_Vision_skew {
   public static function main() {
-    
+    var start:Float = 0, end:Float = 0, sum:Float = 0, best:Float = Math.POSITIVE_INFINITY, worst:Float = 0;
+        var attempts = 2;
+        vision.tools.ImageTools.loadFromFile("https://upload.wikimedia.org/wikipedia/commons/5/50/Vd-Orig.png", function(image)
+        {
+            for (i in 0...attempts)
+            {
+                start = haxe.Timer.stamp();
+                image.skew(90);
+                end = haxe.Timer.stamp();
+                if (end - start > worst) worst = end - start;
+                if (end - start < best) best = end - start;
+                sum += end - start;
+            }
+            trace("----------vision.Vision.invert()----------");
+            trace("attempts: " + attempts);
+            trace("worst: " + worst);
+            trace("best: " + best);
+            trace("average: " + sum / attempts);
+            trace("------------------------------------------");
+        });
   }
 }
