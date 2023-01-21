202c041f8bd7002eb8fb1c85332a9058f6d669fa
Shahar Marcus <88977041+ShaharMS@users.noreply.github.com>
2023-01-21 18:30:01 +0200

    displaced getInclusionMethod() into constructor
    looks like java, jvm and c# still complain about `this` access

--- src/vision/exceptions/LibraryRequired.hx
+++ src/vision/exceptions/LibraryRequired.hx
@@ -2,18 +2,19 @@ package vision.exceptions;
 
 class LibraryRequired extends VisionException {
     public function new(library:String, dependencies:Array<String>, classDotField:String, ?fieldType:String = "function") {
+        
+        function getInclusionMethod(libs:Array<String>):String {
+            #if (lime || openfl || flixel)
+            return '${[for (lib in libs) '\t\t<haxelib name="$lib"/>\n'].join("")}';
+            #elseif kha
+            return '${[for (lib in libs) '\t\tproject.addLibrary(\'$lib\');\n'].join("")}';
+            #elseif ceramic
+            return '\t\tplugins:\n${[for (lib in libs) '\t\t\t- $lib\n'].join("")}';
+            #else
+            return '${[for (lib in libs) '\t\t--library $lib\n'].join("")}';
+            #end
+        }
+        
         super('The $fieldType $classDotField requires the ${library} haxelib.\n\tMake sure ${library + if (dependencies.length > 0) " and it's dependencies are" else " is"} installed & included:\n\n${getInclusionMethod([library].concat(dependencies))}' , "Missing Library Required");
-    }
-
-    extern inline function getInclusionMethod(libs:Array<String>):String {
-        #if (lime || openfl || flixel)
-        return '${[for (lib in libs) '\t\t<haxelib name="$lib"/>\n'].join("")}';
-        #elseif kha
-        return '${[for (lib in libs) '\t\tproject.addLibrary(\'$lib\');\n'].join("")}';
-        #elseif ceramic
-        return '\t\tplugins:\n${[for (lib in libs) '\t\t\t- $lib\n'].join("")}';
-        #else
-        return '${[for (lib in libs) '\t\t--library $lib\n'].join("")}';
-        #end
     }
 }
