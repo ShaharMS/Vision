7a6d27979ac8df9365cf4ec47692484d39f6a4d4
Shahar Marcus <88977041+ShaharMS@users.noreply.github.com>
2023-09-11 10:11:50 +0300

    add gamma function & factorial

--- src/vision/tools/MathTools.hx
+++ src/vision/tools/MathTools.hx
@@ -480,6 +480,31 @@ class MathTools {
 		return (abs(cast number) / cast number) > 0;
 	}
 
+	public static function gamma(x:Float):Float {
+        	var g = 7.0, p = [
+				0.99999999999980993, 676.5203681218851, -1259.1392167224028,
+            			771.32342877765313, -176.61502916214059, 12.507343278686905, 
+				-0.13857109526572012, 9.9843695780195716e-6, 1.5056327351493116e-7];
+
+        	if (x < 0.5) return Math.PI / (Math.sin(Math.PI * x) * gamma(1 - x));
+        	else {
+            		x--;
+            		var a = p[0];
+            		var t = x + g + 0.5;
+            		for (i in 1...p.length) {
+                		a += p[i] / (x + i);
+            		}
+            		return Math.sqrt(Math.PI * 2) * Math.pow(t, x + 0.5) * Math.exp(-t) * a;
+        	}
+    	}
+
+	public static function factorial(value:Float, tryImproveApproximation:Bool = true):Float {
+		var val = gamma(value + 1);
+		if (tryImproveApproximation && value == Std.int(value)) return truncate(val, 11);
+		/* `value == Std.int(value)` a little strange, but this is only effective with small positive integers anyway, so shouldn't be a problem */
+		return val;
+	}
+
 	//-----------------------------------------------------------------------------------------
 	// Conversions
 	//-----------------------------------------------------------------------------------------
