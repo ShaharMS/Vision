20fcf57574750bc0aece22aa1bb5a2e5ef06e2e3
ShaharMS <shaharmsecond@gmail.com>
2025-05-29 17:14:24 +0300

    Detector now basically fully works, onto generation

--- tests/generator/src/Detector.hx
+++ tests/generator/src/Detector.hx
@@ -6,9 +6,12 @@ import sys.FileSystem;
 class Detector {
 
     static var packageFinder = ~/^package ([\w.]+)/m;
-    static var classNameFinder = ~/^class (\w+)/m;
+    static var classNameFinder = ~/^(?:class|abstract) (\w+)/m;
     static var staticFunctionFinder = ~/(?:public static inline|public inline static|inline public static|public static) function (\w+)\((.+)\)(?::\w+)?\s*(?:$|{)/m;
     static var staticFieldFinder = ~/(?:public static inline|public inline static|inline public static|public static) (?:var|final) (\w+)\(get, \w+\)/m;
+    static var instanceFieldFinder = ~/(?:public inline|inline public|public) (?:var|final) (\w+)\(get, \w+\)/m;
+    static var instanceFunctionFinder = ~/(?:public inline|inline public|public) function (\w+)\((.+)\)(?::\w+)?\s*(?:$|{)/m;
+
 
     public static function detectOnFile(pathToHaxeFile:String) {
         var pathToHaxeFile = FileSystem.absolutePath(pathToHaxeFile);
@@ -43,12 +46,35 @@ class Detector {
             staticFields.push(fieldName);
         }
 
+        fileContent = originalFileContent;
+
+        var instanceFunctions = new Map<String, String>();
+
+        while (instanceFunctionFinder.match(fileContent)) {
+            var functionName = instanceFunctionFinder.matched(1);
+            var functionParameters = instanceFunctionFinder.matched(2);
+            fileContent = instanceFunctionFinder.matchedRight();
+
+            instanceFunctions.set(functionName, functionParameters);
+        }
+
+        fileContent = originalFileContent;
+
+        var instanceFields = [];
+        while (instanceFieldFinder.match(fileContent)) {
+            var fieldName = instanceFieldFinder.matched(1);
+            fileContent = instanceFieldFinder.matchedRight();
+
+            instanceFields.push(fieldName);
+        }
 
         return {
             packageName: packageName,
             className: className,
             staticFunctions: staticFunctions,
-            staticFields: staticFields
+            staticFields: staticFields,
+            instanceFunctions: instanceFunctions,
+            instanceFields: instanceFields
         }
     }
 }
--- tests/generator/src/Main.hx
+++ tests/generator/src/Main.hx
@@ -3,5 +3,6 @@ package;
 class Main {
     static function main() {
         Generator.generateFromFile("../../src/vision/tools/MathTools.hx", "../tests/MathTools.test.hx");
+        Generator.generateFromFile("../../src/vision/ds/IntPoint2D.hx", "../tests/IntPoint2D.test.hx");
     }
 }
